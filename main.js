75609beb9bb26d9a56431852e8c7b61e2403229f
Rúnar Berg <runar.BaugssonSigridarson@netapp.com>
2020-07-05 16:24:26 -0700

    Load data asynchronously

--- main.js
+++ main.js
@@ -1,72 +1,93 @@
-const API_ROOT = "https://online-go.com/api/v1";
+const API_ROOT = "https://online-go.com/api/v1/";
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, ms);
+  });
+}
+
+class API {
+  constructor() {
+    this.activeRequestRC = 0;
+  }
+
+  async call(method, pathname, query = {}) {
+    let tries = 0;
+    const url = new URL(pathname, API_ROOT);
+
+    for (const [key, value] of Object.entries(query)) {
+      url.searchParams.append(key, value);
+    }
+
+    while (this.activeRequestRC > 10) {
+      await sleep(500);
+    }
+
+    this.activeRequestRC += 1;
+
+    const request = new Request(url, { method });
+    let response = await fetch(request);
+
+    while (response.status === 429 || tries > 20) {
+      tries += 1;
+      await sleep(1000 * tries);
+      response = await fetch(request);
+    }
+
+    this.activeRequestRC -= 1;
+
+    return response.json();
+  }
+
+  get(pathname, query) {
+    return this.call("GET", pathname, query);
+  }
+}
+
+const api = new API();
 
 async function findPlayer(username) {
   if (!username) {
     return null;
   }
 
-  const playersResponse = await fetch(
-    `${API_ROOT}/players?username=${username}`
-  );
-  const playersResult = await playersResponse.json();
+  const { results } = await api.get("players", { username });
 
-  return playersResult.results.find((player) => player.username === username);
-}
-
-async function findTournament(id) {
-  const response = await fetch(`${API_ROOT}/tournaments/${id}/rounds`);
-  return response.json();
+  return results.find((player) => player.username === username);
 }
 
 async function getRounds() {
   const searchParams = new URLSearchParams(window.location.search);
+  const tournamentId = searchParams.get("tournament") || "59567";
   const [player, tournament] = await Promise.all([
     findPlayer(searchParams.get("player")),
-    findTournament(
-      Number.parseInt(searchParams.get("tournament"), 10) || 59567
-    ),
+    api.get(`tournaments/${tournamentId}/rounds`),
   ]);
 
   if (!player) {
     return [];
   }
 
-  return Promise.all(
-    tournament.map(async (round) => {
-      const opponents = round.matches
-        .filter(
-          ({ white, black }) => white === player.id || black === player.id
-        )
-        .map(({ white, black }) => (white === player.id ? black : white));
+  return tournament.map((round) => {
+    const opponents = round.matches
+      .filter(({ black, white }) => black === player.id || white === player.id)
+      .map(({ black, white }) => (black === player.id ? white : black));
 
-      const players = await Promise.all(
-        [player.id, ...opponents].map((id) =>
-          fetch(`${API_ROOT}/players/${id}`).then((response) => response.json())
-        )
-      );
+    const players = [player.id, ...opponents];
 
-      const games = await Promise.all(
-        round.matches
-          .filter(({ white }) => players.some(({ id }) => id === white))
-          .map(({ gameid }) =>
-            fetch(`${API_ROOT}/games/${gameid}`).then((response) =>
-              response.json()
-            )
-          )
-      );
+    // Filter out just the games that was played in this group.
+    const games = round.matches.filter(({ black }) =>
+      players.some((id) => id === black)
+    );
 
-      return {
-        round: round.round_number,
-        players: players.map((player) => ({
-          player,
-          games: games.filter(
-            ({ players }) =>
-              players.white.id === player.id || players.black.id === player.id
-          ),
-        })),
-      };
-    })
-  );
+    return {
+      round: round.round_number,
+      players: players.map((id) => ({
+        id,
+        games: games.filter(({ black, white }) => black === id || white === id),
+      })),
+    };
+  });
 }
 
 function getResult(gamedata) {
@@ -103,6 +124,21 @@ function fillFragmentData(fragment, data) {
       })
     );
   }
+
+  for (const template of fragment.querySelectorAll("[data-await]")) {
+    const gettingData = data[template.dataset.await];
+
+    if (!gettingData) {
+      return;
+    }
+
+    const fragment = template.content.cloneNode(true);
+
+    gettingData.then((data) => {
+      fillFragmentData(fragment, data);
+      template.after(fragment);
+    });
+  }
 }
 
 async function main() {
@@ -117,26 +153,30 @@ async function main() {
         roundNumber: round.round,
         players: {
           items: round.players,
-          childData: (player) => ({
-            username: player.player.username,
-            playerHref: `https://online-go.com/player/${player.player.id}`,
-            games: {
-              items: player.games,
-              childData: (game) => {
-                const isBlack = game.players.black.id === player.player.id;
-                const opponent = isBlack
-                  ? game.players.white
-                  : game.players.black;
+          childData: ({ id: playerId, games }) => ({
+            fetchingPlayer: api.get(`players/${playerId}`).then((player) => ({
+              username: player.username,
+              playerHref: `https://online-go.com/player/${playerId}`,
+              games: {
+                items: games,
+                childData: ({ gameid: gameId }) => ({
+                  fetchingGame: api.get(`games/${gameId}`).then((game) => {
+                    const isBlack = game.players.black.id === playerId;
+                    const opponent = isBlack
+                      ? game.players.white
+                      : game.players.black;
 
-                return {
-                  color: isBlack ? "Black" : "White",
-                  gameHref: `https://online-go.com/game/${game.id}`,
-                  opponent: opponent.username,
-                  opponentHref: `https://online-go.com/player/${opponent.id}`,
-                  result: getResult(game.gamedata),
-                };
+                    return {
+                      color: isBlack ? "Black" : "White",
+                      gameHref: `https://online-go.com/game/${gameId}`,
+                      opponent: opponent.username,
+                      opponentHref: `https://online-go.com/player/${opponent.id}`,
+                      result: getResult(game.gamedata),
+                    };
+                  }),
+                }),
               },
-            },
+            })),
           }),
         },
       }),
